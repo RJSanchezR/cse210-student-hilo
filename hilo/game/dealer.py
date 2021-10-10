0b352de19089036a0edfe33cc94ceb439b736ebe
RJSanchezR <san16071@byui.edu>
2021-10-09 23:28:03 -0600

    tried with return

--- hilo/game/dealer.py
+++ hilo/game/dealer.py
@@ -14,20 +14,21 @@ class Dealer:
         """
         self.score = 300
         self.new_card = 0
-        # The current card will always be the new card before it changes
         self.current_card = self.new_card
 
-    
     def draw_card(self):
         """Assign a random card (number) to the new card
         
         Args:
             Self (Dealer): An instance of Dealer.
+
+        Returns:
+            number: The value of the new card.
         """
 
         self.new_card = random.randint(1, 13)
+        return self.new_card
 
-    
     def get_guess(self):
         """Determines if the user can throw again. Returns True or False.
 
@@ -39,8 +40,7 @@ class Dealer:
         """
         user_guess = input("Do you think the next card will be higher or lower? [h/l]: ")
 
-        return user_guess
-    
+        return user_guess    
 
     def calculate_points(self, guess):
         """Calculates the number of points after the guess. 100 points 
@@ -63,7 +63,6 @@ class Dealer:
                 return 100
             else:
                 return (-75)
-
     
     def can_play(self):
         """Determines whether or not the Dealer can draw a card according
